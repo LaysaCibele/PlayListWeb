c8f095d88b271b11d97091029e9282fbfd3f0593
Laysa <laysacibele23@gmail.com>
2026-05-14 15:14:57 -0300

    feat: Models da página específica

--- jogos/models.py
+++ jogos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Jogo(models.Model):
+    titulo = models.CharField(max_length=150)
+    descricao = models.TextField()
+    genero = models.CharField(max_length=100)
+    plataforma = models.CharField(max_length=100)
+    desenvolvedora = models.CharField(max_length=100)
+    anoLancamento = models.IntegerField() 
+    linkWiki = models.URLField(max_length=250, blank=True, null=True)
+    linkMapa = models.URLField(max_length=250, blank=True, null=True)
+    anotacoes = models.TextField(blank=True, null=True)
+    capaImagem = models.ImageField(upload_to='capas_jogos/', blank=True, null=True)
+
+    def __str__(self):
+        return self.titulo
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('cadastro/', views.cadastro_page, name='cadastro'),
     path('catalogo/', views.catalogo_page, name='catalogo'),
